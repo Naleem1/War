a9d8113817540fb6220dbe08c4968cfeed87caf6
Najee Aleem <najeez.@Najees-MacBook-Pro.local>
2022-01-05 15:18:43 -0500

    Deck Created

--- README.md
+++ README.md
@@ -1,31 +1,22 @@
 # War
 War card game
 
-HTML
--create divs for card shape and value 
--add secondary header for rules  
+HTML {
+-create divs and button 
+}
 
-JavaScript
-Start game
--2 players
--deck of cards 
+JavaScript {
+-cards
+-deck
+-shuffle
+-deal cards 
+-players
+}
 
-Dealing cards
--create deck 
--shuffle deck 
--distribute deck evenly 
+play {
+-both players play cards from top of deck and high card wins 
+}
 
-Play/Turn
--Revel each card in turn (battle phase)
--determine the outcome (winner recieves all cards to bottom of deck)
--if a tie happens commence WAR
-
-War
--player will place three cards face down and one facing up (repeat action until winner is determined)
--winning player recieves all cards 
-
-ending parameters 
--player runs out of cards 
 
 
 CSS
--- game.js
+++ game.js
@@ -1,11 +1,48 @@
+console.log('this is going to WORK!')
+const startButton = document.querySelector('.deal')
+console.log(startButton)
 
 
-class Card{
-    constructor(suit,number) {
-        this.suit=suit,
-        this.number=number
+// I need cards and players 
+const VALUES = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+const SUITS = ['Hearts', 'Diamonds','Clubs','Spades'];
+const player1 = []
+const player2 = []
+let deck = []
+
+// I need a card object 
+class Card {
+    constructor(value,suit) {
+        this.value = value,
+        this.suit = suit
     }
 }
+// I need a deck object with fuctions to shuffle and deal
+function shuffleDeck () {
+    console.log('shuffleDeck')
+}
 
-let card = new Card('Heart',2);
-console.log(card)
+function createDeck (){
+    deck = [];
+        for (let i = 0; i < SUITS.length; i++) {
+            for (let j = 0; j < VALUES.length; j++) {
+                // console.log(VALUES[j], SUITS[i])
+                const card = {
+                    value: VALUES[j],
+                    suit: SUITS[i]
+                }
+                deck.push(card)
+            }
+        }
+// console.log(deck)
+shuffleDeck()
+}
+startButton.addEventListener('click', createDeck)
+// createDeck()
+// console.log(deck)
+
+// I need to have win parameters 
+
+
+
+
