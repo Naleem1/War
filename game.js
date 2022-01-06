71bc061233236d4e69201875a86e5066b8230597
Najee Aleem <najeez.@Najees-MacBook-Pro.local>
2022-01-06 17:28:27 -0500

    cards and shuffle

--- game.js
+++ game.js
@@ -1,32 +1,33 @@
 console.log('this is going to WORK!')
-const startButton = document.querySelector('.deal')
+
+const startButton = document.querySelector('#deal')
 console.log(startButton)
 
+const resetButton = document.querySelector('#reset')
+console.log(resetButton)
 
-// I need cards and players 
+
+
 const VALUES = [1,2,3,4,5,6,7,8,9,10,11,12,13];
 const SUITS = ['Hearts', 'Diamonds','Clubs','Spades'];
 const player1 = []
 const player2 = []
 let deck = []
 
-// I need a card object 
+
 class Card {
     constructor(value,suit) {
         this.value = value,
         this.suit = suit
     }
 }
-// I need a deck object with fuctions to shuffle and deal
-function shuffleDeck () {
-    console.log('shuffleDeck')
-}
+
 
 function createDeck (){
     deck = [];
         for (let i = 0; i < SUITS.length; i++) {
             for (let j = 0; j < VALUES.length; j++) {
-                // console.log(VALUES[j], SUITS[i])
+
                 const card = {
                     value: VALUES[j],
                     suit: SUITS[i]
@@ -34,14 +35,26 @@ function createDeck (){
                 deck.push(card)
             }
         }
-// console.log(deck)
-shuffleDeck()
-}
-startButton.addEventListener('click', createDeck)
-// createDeck()
-// console.log(deck)
+        shuffleDeck()
+        console.log(deck)
 
-// I need to have win parameters 
+}
+
+function shuffleDeck(){
+    let shuffleDeck = []
+    while (deck.length > 0){
+        let randomIndex = Math.floor(deck.length * Math.random());
+    
+        shuffleDeck.push(deck[randomIndex])
+        deck.splice(randomIndex, 1)
+    }
+    deck = shuffleDeck
+}
+
+
+startButton.addEventListener('click', createDeck)
+console.log(deck)
+
 
 
 
